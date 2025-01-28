68073ce64e9f9786a18426353c75400d3b909608
PaoloIncollu <paoloinco@gmail.com>
2025-01-28 20:17:21 +0100

    corretta chiamata axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,35 +14,46 @@ export default {
         this.getPerfumes();
     },
     methods: {
-        async getPerfumes(url = 'http://127.0.0.1:8000/api/perfumes') {
-            this.clickedButton = true;
-            try {
-                const res = await axios.get(url);
-                this.perfumes = res.data.perfumes.data;
-                this.prevPage = res.data.perfumes.prev_page_url;
-                this.nextPage = res.data.perfumes.next_page_url;
-                console.log(this.perfumes);
-            } catch (error) {
-                console.error('Errore durante il caricamento dei profumi:', error);
-            } finally {
+        getPerfumes(url = 'http://127.0.0.1:8000/api/perfumes') {
+            axios.get(url)
+                .then(res => {
+                    this.perfumes = res.data.perfumes.data;
+                    this.prevPage = res.data.perfumes.prev_page_url;
+                    this.nextPage = res.data.perfumes.next_page_url;
+
+                    this.perfumes.forEach(perfume => {
+                        if (perfume.img) {
+                            if (perfume.img.startsWith('http') || perfume.img.startsWith('https')) {
+                                perfume.img = perfume.img;
+                            } else {
+                                perfume.img = `http://127.0.0.1:8000/storage/${perfume.img}`;
+                            }
+                        } else {
+                            perfume.img = this.src;
+                        }
+                    });
+                });
+        },
+
+        ToPrevPage() {
+            if (this.prevPage && !this.clickedButton) {
+                this.clickedButton = true;
+                this.getPerfumes(this.prevPage);
                 this.clickedButton = false;
             }
         },
 
-        ToPrevPage() {
-            if (this.prevPage) {
-                this.getPerfumes(this.prevPage);
-            }
-        },
-
         ToNextPage() {
-            if (this.nextPage) {
+            if (this.nextPage && !this.clickedButton) {
+                this.clickedButton = true;
                 this.getPerfumes(this.nextPage);
+                this.clickedButton = false;
             }
         }
     }
 };
 </script>
+
 
 <template>
     <main>
@@ -67,10 +78,10 @@ export default {
 
             <div class="row" id="perfumes">
 
-                <div class="col-sm-12 col-md-6 col-lg-3 mb-3" v-for="(perfume, index) in perfumes" :key="perfume.id || index">
+                <div class="col-sm-12 col-md-6 col-lg-3 mb-5" v-for="perfume in perfumes" :key="perfume.id">
 
-                    <div class="my-card card p-2 align-self-stretch flex-grow-2 mx-0">
-                        <img class="card-img-top h-50" :src="perfume.img" :alt="perfume.name_perfume">
+                    <div class="my-card card d-flex  p-2 align-self-stretch flex-grow-2 mx-0">
+                        <img class="card-img-top h-300" :src="perfume.img" :alt="perfume.name_perfume">
 
                         <div class="card-body text-center">
                             <h6 class="card-title mb-3 fw-bold">{{ perfume.name_perfume }}</h6>
